ba1cf45cac05287055397c79ca855ae0e30dd8fb
Ludwring Liccien <ludwring.liccien@gmail.com>
2024-04-23 15:03:33 -0500

    ci: set base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://lliccien.github.io/app-github-repos",
   plugins: [react()],
   server: {
     proxy: {
