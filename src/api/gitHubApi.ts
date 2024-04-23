83ab57706cab46e5c2acd9a80c55a88816f48a0e
Ludwring Liccien <ludwring.liccien@gmail.com>
2024-04-23 15:37:44 -0500

    ci: remove server proxy

--- src/api/gitHubApi.ts
+++ src/api/gitHubApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const gitHub = axios.create({
-  baseURL: "/github",
+  baseURL: "https://github.com",
   headers: {
     Accept: "application/json",
   },
@@ -16,7 +16,7 @@ gitHub.interceptors.request.use((config) => {
 });
 
 export const gitHubApi = axios.create({
-  baseURL: "/api",
+  baseURL: "https://api.github.com",
   headers: {
     Accept: "application/json",
   },
