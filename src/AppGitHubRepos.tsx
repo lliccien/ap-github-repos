4c4c9a7c112de6a8ce7591e11de23f386b6625c2
Ludwring Liccien <ludwring.liccien@gmail.com>
2024-04-24 15:05:21 -0500

    feat: add Auth provider whith usecontext

--- src/AppGitHubRepos.tsx
+++ src/AppGitHubRepos.tsx
@@ -1,10 +1,11 @@
+import { AuthProvider } from "./context/AuthProvider";
 import { AppRouter } from "./router/AppRouter";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <AppRouter />
-    </>
+    </AuthProvider>
   );
 }
 
--- src/router/PublicRouter.tsx
+++ src/router/PublicRouter.tsx
@@ -1,16 +1,14 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 import { Navigate } from "react-router-dom";
+import { UserContext } from "../context/AuthProvider";
 
 interface pros {
   children: React.ReactNode;
 }
 
 export const PublicRouter: FC<pros> = ({ children }) => {
-  const isAuthenticated =
-    sessionStorage.getItem("token") != undefined ||
-    sessionStorage.getItem("token") != null
-      ? true
-      : false;
+  const { user } = useContext(UserContext);
+  const isAuthenticated = user.isAuthenticated;
 
   return !isAuthenticated ? <>{children}</> : <Navigate to="/home" />;
 };
